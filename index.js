db9f54b5955fb4251161f04b696af91f62540e21
Divya Dwivedi <divya.dwivedi.mnnit@gmail.com>
2023-05-11 05:03:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const dbConnect = require("./dbConnect");
+const authRouter = require("./routers/authRouter");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const cloudinary = require("cloudinary").v2;
+
+dotenv.config("./.env");
+
+// Configuration
+cloudinary.config({
+    secure: true,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+
+//middlewares
+app.use(express.json({ limit: "10mb" }));
+app.use(morgan("common"));
+app.use(cookieParser());
+let origin = 'http://localhost:3000';
+console.log('here env', process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'production') {
+    origin = process.env.CLIENT_ORIGIN;
+}
+app.use(
+    cors({
+        credentials: true,
+        origin
+    })
+);
+
+app.use("/auth", authRouter);
+app.get("/", (req, res) => {
+    res.status(200).send("OK from Server");
+});
+
+const PORT = process.env.PORT || 4000;
+
+dbConnect();
+if(process.env.NODE_ENV=='production'){
+    app.use(express.static("client/build"))
+}
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`);
+});
